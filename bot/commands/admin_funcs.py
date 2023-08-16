c7f5698bdb116e8233dde34ea1a10e45f388c29c
Ilya Dobrynin <mr.woodysimpson@gmail.com>
2023-08-16 09:47:55 +0300

    added drop groups

--- bot/commands/admin_funcs.py
+++ bot/commands/admin_funcs.py
@@ -15,7 +15,7 @@ def start_of_school_year():
 
 
 async def processor(user: User, message: Message, tg_client: TgClient, callback_query=False, stage=None):
-    if message.button == 'drop_groups_new_year':
+    if message.button == 'drop_groups_new_year' or message.text == "drop_groups_new_year":
         print("ОЧИСТКА НОМЕРОВ ГРУПП СТАРОГО УЧЕБНОГО ГОДА")
         sql = "UPDATE tg_users SET groupid = 0"
         cursor.execute(sql)
