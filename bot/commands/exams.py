2c678eefe9ec6e676c565fa0424a8327f61ae0b4
Ilya Dobrynin <mr.woodysimpson@gmail.com>
2023-11-26 10:32:31 +0300

    added exams page

--- bot/commands/exams.py
+++ bot/commands/exams.py
@@ -6,18 +6,20 @@ from ..BotClasses.Keyboards import keyboard
 from ..BotClasses.Stage_handler import Stage
 from clients.tg.api import TgClient
 
+button_template = [[['Открыть', '']]]
 
+app = "https://t.me/knrtukaibot/exams?startapp=groupid%3D{}"
 async def processor(user: User, message: Message, tg_client: TgClient, callback_query=False, stage=None):
-    stage = Stage(user, message)
-    stage._set_status(0)
-    await tg_client.send_message(user.id, 'Главное меню', buttons=keyboard('main_keyboard', user).get_keyboard())
+    button = button_template
+    button[0][0][1] = app.format(user.group_id)
+    await tg_client.send_message(user.id, 'Твои экзамены:', buttons=keyboard('attestation', user, buttons=button).get_link())
     await tg_client.answer_callback_query(message.callback_query_id)
     return
 
 
 command = command_class()
 
-command.keys = ['выход', 'главное меню', 'меню', 'обновить']
+command.keys = ['экзамены', 'сессия']
 command.process = processor
 command.role = [1]
-command.payload = ['exit', 'main_menu']
+command.payload = ['exams']
