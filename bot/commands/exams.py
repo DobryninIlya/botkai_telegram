f613706b25d45439a2a9c72cd52b72932fd3f7d7
Ilya Dobrynin <mr.woodysimpson@gmail.com>
2025-01-24 14:28:57 +0300

    fixed exams

--- bot/commands/exams.py
+++ bot/commands/exams.py
@@ -11,7 +11,7 @@ button_template = [[['Открыть', '']]]
 app = "https://t.me/knrtukaibot/exams?startapp=groupid%3D{}"
 async def processor(user: User, message: Message, tg_client: TgClient, callback_query=False, stage=None):
     button = button_template
-    button[0][0][1] = app.format(user.group_id)
+    button[0][0][1] = app.format(user.group_name)
     await tg_client.send_message(user.id, 'Твои экзамены:', buttons=keyboard('attestation', user, buttons=button).get_link())
     await tg_client.answer_callback_query(message.callback_query_id)
     return
